593df25ef96e9719e59e4b531fb0816799b3edb0
Josephchinedu <47852925+Josephchinedu@users.noreply.github.com>
2020-11-24 20:17:34 +0100

    Update config.php

--- user/config.php
+++ user/config.php
@@ -2,13 +2,13 @@
 <?php
 	
 	// Set up a connection
-	$connect = mysqli_connect("localhost", "nextiftk_zari", "Ability_2020");
+	$connect = mysqli_connect("r1bsyfx4gbowdsis.cbetxkdyhwsb.us-east-1.rds.amazonaws.com", "y1e7tisi0h1tg8fd", "ms4wzdfeiz00muiv");
 	if(!$connect){
 		echo "Connection failed" .mysqli_error();	
 	}
 
 	// Connect to a database
-	$connected = mysqli_select_db($connect, "nextiftk_zari");
+	$connected = mysqli_select_db($connect, "nkavg75tgv3mnjsi");
 	if(!$connected){
 		
 		echo "Database Selection failed" .mysqli_error();
