ab3892ed984cc669654328a63ba8da2bc4626ba9
Josephchinedu <47852925+Josephchinedu@users.noreply.github.com>
2020-11-24 17:52:44 +0100

    Update contact.php

--- user/contact.php
+++ user/contact.php
@@ -7,7 +7,7 @@ if (isset($_POST['submit'])) {
     $message = $_POST['message'];
     
     
-    $mailTo = "hello@nextpredict.com";
+    $mailTo = "joseph@digitaldesign247.com";
     $headers = "From: ".$mailFrom;
     $txt = "You've received an email from ".$name.".\n\n\n".$message;
     
